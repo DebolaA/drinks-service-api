0fd5f9113103fc6c1afb2f7899adac4afe25c2bd
Debola Adedapo <niikee80@hotmail.com>
2024-03-08 12:03:58 +0000

    feat: Implement GET request for home endpoint and test

---
+++ src/controllers/home_request.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import { app } from '../app'
+describe('Test home API endpoint request', () => {
+    // test('Red - Test should fail', async () => {
+    //     const res = await request(app).get('/')
+    //     expect(res.statusCode).toEqual(200)
+    //     expect(res.text).to('This should fail!')
+    // })
+
+    test('GET should return correct message', async () => {
+        const res = await request(app).get('/')
+        expect(res.statusCode).toEqual(200)
+        expect(res.text).toEqual('Welcome to the Drinks API!')
+    })
+})
