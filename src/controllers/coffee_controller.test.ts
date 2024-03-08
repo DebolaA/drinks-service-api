5dda857acb7908afae5dcf0655caff120a78214f
Debola Adedapo <niikee80@hotmail.com>
2024-03-08 13:12:34 +0000

    feat: More test for coffee endpoint

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -1,11 +1,32 @@
 import request from 'supertest'
 import { app } from '../app'
 
-test('GET /coffee should return correct object', async () => {
-    const res = await request(app).get('/coffee').query({ coffeeName: 'Latte' })
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual({
-        drinkType: 'Coffee',
-        name: 'Latte',
+describe('Test coffeelover API endpoint request', () => {
+    test('GET /coffee should return correct object', async () => {
+        const res = await request(app)
+            .get('/coffee')
+            .query({ coffeeName: 'Latte' })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Latte',
+        })
+    })
+
+    test('GET /coffee with coffeeName Macchiato', async () => {
+        const res = await request(app)
+            .get('/coffee')
+            .query({ coffeeName: 'Macchiato' })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Macchiato',
+        })
+    })
+
+    test('GET Request to the /coffeelover endpoint should return correct message', async () => {
+        const res = await request(app).get('/coffeelover')
+        expect(res.statusCode).toEqual(200)
+        expect(res.text).toEqual('I like coffee!')
     })
 })
